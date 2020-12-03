10be38406fb221c16ba56010065443e431cf444e
Frenken Flores <fflores@su-a2.msk.21-school.ru>
2020-12-03 20:31:30 +0300

    update #6

--- setup.sh
+++ setup.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-
-
-echo "Launching minikube ..."
-minikube --vm-driver=docker start --extra-config=apiserver.service-node-port-range=1-35000
-eval $(minikube docker-env)
-echo "Minikube is launched"
-echo "Enable Ingress ..."
+minikube --vm-driver=virtualbox start --extra-config=apiserver.service-node-port-range=1-35000
 minikube addons enable ingress
-echo "Verify Ingress ..."
-kubectl get pods -n kube-system
-echo "Ingress verified"
-echo "Enable dashboard ..."
-minikube addons enable dashboard
-echo "dashboard enabled"
-echo "Building images ..."
-docker build -t nginx ./src/nginx/
+docker build -t service_nginx ./srcs/nginx
