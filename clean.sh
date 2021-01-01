fb91b0808130fe5806e50d0ceb71794c6e7f5bea
FrenkenFlores <fflores@student.21-school.ru>
2021-01-01 15:33:08 +0300

    update #12: share single ip addres for all services

--- clean.sh
+++ clean.sh
@@ -2,14 +2,15 @@
 
 # This script deletes everything, useful for rebuilding Docker images with Minikube context
 
-#kubectl delete --all ingresses
-#kubectl delete --all deployments
-#kubectl delete --all pods
-#kubectl delete --all services
-#kubectl delete --all pvc
-kubectl delete deployment wordpress-mysql
-kubectl delete svc wordpress-mysql
-kubectl delete pvc mysql-pv-claim
-docker rmi mysql-image
-docker build ./srcs/mysql/ -t mysql-image
-kubectl apply -f ./srcs/configs/mysql.yaml
+kubectl delete --all ingresses
+kubectl delete --all deployments
+kubectl delete --all pods
+kubectl delete --all services
+kubectl delete --all pvc
+kubectl delete --all configmap 
+#kubectl delete deployment wordpress-mysql
+#kubectl delete svc wordpress-mysql
+#kubectl delete pvc mysql-pv-claim
+#docker rmi mysql-image
+#docker build ./srcs/mysql/ -t mysql-image
+#kubectl apply -f ./srcs/configs/mysql.yaml
--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-minikube --vm-driver=virtualbox start --extra-config=apiserver.service-node-port-range=1-35000
+#minikube --vm-driver=virtualbox start --extra-config=apiserver.service-node-port-range=1-35000
 minikube addons enable metallb
 minikube addons enable dashboard
 eval $(minikube docker-env)
 docker build -t nginx-image ./srcs/nginx
 #docker build -t ftps-image ./srcs/ftps
 docker build -t mysql-image ./srcs/mysql
-docker build -t wordpress-image ./srcs/wordpress
 #docker build -t phpmyadmin-image ./srcs/phpmyadmin
 #docker build -t influxdb-image ./srcs/influxdb
 #docker build -t grafana-image ./srcs/grafana
-kubectl create -f ./srcs/configs/metallb.yaml
-#kubectl create -f ./srcs/configs/nginx.yaml
-#kubectl create -f ./srcs/configs/mysql.yaml
-#kubectl create -f ./srcs/configs/wordpress.yaml
-kubectl create -k ./srcs/configs/
+kubectl apply -f ./srcs/configs/metallb.yaml
+kubectl apply -f ./srcs/configs/nginx.yaml
+kubectl apply -f ./srcs/configs/mysql.yaml
+#kubectl create -k ./srcs/configs/
+kubectl get svc | awk '{print $4}' | grep 192 | awk 'NR==1' > ./srcs/wordpress/ip.txt
+docker build -t wordpress-image ./srcs/wordpress
+kubectl apply -f ./srcs/configs/wordpress.yaml
