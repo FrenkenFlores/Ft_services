41e1ce56f27e9488a2a19b5ac78ada7fcbb2bc23
FrenkenFlores <fflores@student.21-school.ru>
2021-01-03 02:07:23 +0300

    update #14: changed php servers to nginx with FastCGI, finished phpmyadmin. LEMP stack is ready

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ https://wiki.alpinelinux.org/wiki/Production_DataBases_:_mysql
 https://wiki.alpinelinux.org/wiki/PhpMyAdmin
 https://docs.phpmyadmin.net/en/latest/
 https://wiki.alpinelinux.org/wiki/Nginx
+https://wiki.alpinelinux.org/wiki/Nginx_with_PHP
 https://wiki.alpinelinux.org/wiki/WordPress
 https://wiki.alpinelinux.org/wiki/Main_Page
 https://metallb.universe.tf/usage/
+nginx conf for php-fpm : https://github.com/christophelimpalair/nginx_series/blob/master/Episode_4.md
+
+https://192.168.99.128/phpmyadmin/doc/html/index.html
+13: Permission denied : https://qna.habr.com/q/127571
--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -2,7 +2,14 @@
 #Retrieve external IP
 IP=$(cat /ip.txt)
 #Replace IP
+sed -i "s/user = nobody/user = nginx/g" /etc/php7/php-fpm.d/www.conf
+sed -i "s/group = nobody/group = nginx/g" /etc/php7/php-fpm.d/www.conf
+sed -i "s/;listen.owner = nobody/listen.owner = nginx/g" /etc/php7/php-fpm.d/www.conf
+sed -i "s/;listen.group = nginx/listen.group = nginx/g" /etc/php7/php-fpm.d/www.conf
 sed -i "s/localhost/$IP/g" /www/wordpress/wp-config.php
+chown -R nginx:nginx 0700 /var/lib/nginx/
 #Launch
 #php --server 0.0.0.0:5050 --docroot /www/wordpress/
-php -S 0.0.0.0:5050 -t /www/wordpress/
+#php -S 0.0.0.0:5050 -t /www/wordpress/
+php-fpm7
+nginx -g 'daemon off;'
