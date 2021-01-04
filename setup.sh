dcdc8ed423ac543cc0db261fa717eac54f81e959
FrenkenFlores <fflores@student.21-school.ru>
2021-01-04 16:16:03 +0300

    update #16.2

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ minikube addons enable metallb
 minikube addons enable dashboard
 eval $(minikube docker-env)
 docker build -t nginx-image ./srcs/nginx
-#docker build -t ftps-image ./srcs/ftps
+docker build -t ftps-image ./srcs/ftps
 docker build -t mysql-image ./srcs/mysql
 docker build -t phpmyadmin-image ./srcs/phpmyadmin
 #docker build -t influxdb-image ./srcs/influxdb
@@ -13,6 +13,7 @@ kubectl apply -f ./srcs/configs/metallb.yaml
 kubectl apply -f ./srcs/configs/nginx.yaml
 kubectl apply -f ./srcs/configs/mysql.yaml
 kubectl apply -f ./srcs/configs/phpmyadmin.yaml
+kubectl apply -f ./srcs/configs/ftps.yaml
 #kubectl create -k ./srcs/configs/
 kubectl get svc | awk '{print $4}' | grep 192 | awk 'NR==1' > ./srcs/wordpress/ip.txt
 docker build -t wordpress-image ./srcs/wordpress
